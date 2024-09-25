a109d728fd8c36fff879e10e24709b642511d8fe
kinto-gota-hasegawa <gota.hasegawa@kinto-technologies.com>
2024-09-25 20:37:53 +0900

    Add navigation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    // Jetpack Compose integration
+    implementation(libs.androidx.navigation.compose)
 }
